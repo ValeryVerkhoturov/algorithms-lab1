ef42788d99aa0d2e50a96773223d9706d6ee89f3
Valery Verkhoturov <verkhoturov.valery@icloud.com>
2021-10-18 18:49:48 +0300

    add lab2

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -8,9 +8,25 @@ namespace Lab2
         static void Main(string[] args)
         {
             WeightedDirectedGraph weightedDirectedGraph =
-                new WeightedDirectedGraph(10);
-            weightedDirectedGraph.AddVertex(new Edge(10, 2, 3));
-            Console.WriteLine(weightedDirectedGraph.ToStringWithoutDirection());
+                new WeightedDirectedGraph(7);
+            // weightedDirectedGraph.AddEdge(1,2, 1);
+            // weightedDirectedGraph.AddEdge(1,4, 2);
+            // weightedDirectedGraph.AddEdge(4,2, 3);
+            // weightedDirectedGraph.AddEdge(2,5, 4);
+            // weightedDirectedGraph.AddEdge(5,4, 5);
+            // weightedDirectedGraph.AddEdge(3,5, 6);
+            // weightedDirectedGraph.AddEdge(3,6, 7);
+            // weightedDirectedGraph.AddEdge(6,6, 8);
+            weightedDirectedGraph.AddEdge(2,1, 1);
+            weightedDirectedGraph.AddEdge(1,4, 2);
+            weightedDirectedGraph.AddEdge(2,4, 3);
+            weightedDirectedGraph.AddEdge(5,2, 4);
+            weightedDirectedGraph.AddEdge(4,5, 5);
+            weightedDirectedGraph.AddEdge(3,5, 6);
+            weightedDirectedGraph.AddEdge(3,6, 7);
+            weightedDirectedGraph.AddEdge(6,6, 8);
+            Console.WriteLine(weightedDirectedGraph);
+            weightedDirectedGraph.BreadthFirstTraversal(3);
         }
     }
 }
--- Lab2/WeightedDirectedGraph.cs
+++ Lab2/WeightedDirectedGraph.cs
@@ -1,57 +1,102 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Linq;
+using System.Diagnostics;
 
 namespace Lab2
 {
     public class WeightedDirectedGraph
     {
-        // в каждой ячейке  матрицы соответствующим координатам
-        // лежит ребро с направлением (откуда, куда) и вес
-        private Edge[,] AdjacencyMatrix;
+        private int[,] AdjacencyMatrix { get; }
+        private List<int> Vertexes { get; }
 
         public WeightedDirectedGraph(int size)
         {
-            AdjacencyMatrix = new Edge[size, size];
+            Vertexes = new List<int>();
+            AdjacencyMatrix = new int[size, size];
             for (int i = 0; i < AdjacencyMatrix.GetLength(0); i++)
                 for (int j = 0; j < AdjacencyMatrix.GetLength(1); j++)
-                    AdjacencyMatrix[i, j] = new Edge(0, -1, -1);
+                    AdjacencyMatrix[i, j] = 0;
         }
 
-        public void AddVertex(Edge edge)
+        public void AddEdge(int from, int to, int weight)
         {
-            int x = edge.From;
-            int y = edge.To;
-            if (x > AdjacencyMatrix.Length || y > AdjacencyMatrix.Length)
+            if (from > AdjacencyMatrix.GetLength(
+                0) || to > AdjacencyMatrix.GetLength(1))
                 throw new ArgumentOutOfRangeException();
-            AdjacencyMatrix[x, y] = edge;
-            AdjacencyMatrix[y, x] = edge;
+            AdjacencyMatrix[from, to] = weight;
+            if (!Vertexes.Contains(from))
+                Vertexes.Add(from);
+            if (!Vertexes.Contains(to))
+                Vertexes.Add(to);
         }
 
-        public string ToStringWithoutDirection()
+        public int CountVertexes()
         {
-            string output = "";
-                for (int i = 0; i < AdjacencyMatrix.GetLength(0); i++)
-                {
-                    output += "[ ";
-                    for (int j = 0; j < AdjacencyMatrix.GetLength(1); j++)
-                        output += AdjacencyMatrix[i, j].Weight + ", ";
-                    output += "]\n";
-                }
-            return output;
+            return Vertexes.Count;
         }
-        
+
+        public int[] BreadthFirstTraversal(int vertex)
+        {
+            if (!Vertexes.Contains(vertex))
+                throw new ArgumentException();
+            var mark = new bool[CountVertexes()]; // visited
+            var parent = new int[CountVertexes()];
+            for (int i = 0; i < CountVertexes(); i++)
+            {
+                mark[i] = false;
+                parent[i] = 0;
+            }
+
+            Queue<int> queue = new Queue<int>();
+            mark[vertex] = true;
+            queue.Enqueue(vertex);
+            while (queue.Count > 0)
+            {
+                vertex = queue.Dequeue();
+                for (int i = 0; i < CountVertexes(); i++)
+                {
+                    if (AdjacencyMatrix[vertex, i] == 0 || mark[i])
+                        continue;
+                    mark[i] = true;
+                    queue.Enqueue(i);
+                    parent[i] = vertex;
+                    // Console.Write($"{i} ");
+                }
+
+                mark[vertex] = true;
+            }
+            
+            foreach (var VARIABLE in parent)
+            {
+                Console.Write($"{VARIABLE} ");
+            }
+            PrintPath(parent, 2, 4);
+            return parent;
+        }
+
+        public void PrintPath(int[] parent, int begin, int end)
+        {
+            int u = parent[end];
+            if (u == begin)
+            {
+                Console.Write(" {0} {1} ", u, end);
+                return;
+            }
+            else if (parent[end] == 0)
+            {
+                Console.WriteLine(" Пути из {0} в {1} нет", begin, end);
+            }
+            else PrintPath(parent, begin, u);
+            Console.Write("{0} ", end);
+        }
         public override string ToString()
         {
             string output = "";
             for (int i = 0; i < AdjacencyMatrix.GetLength(0); i++)
             {
-                output += "[";
+                output += "[ ";
                 for (int j = 0; j < AdjacencyMatrix.GetLength(1); j++)
-                {
-                    output += AdjacencyMatrix[i, j].ToString();
-                }
+                    output += AdjacencyMatrix[i, j].ToString() + " ";
                 output += "]\n";
             }
             
